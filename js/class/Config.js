7ace791d752c9f27c4277c206c99e8261a1d00a7
leone <709890764@qq.com>
2024-12-11 21:59:20 +0800

    更改初始化配置及增加一些特殊单字练习

--- js/class/Config.js
+++ js/class/Config.js
@@ -47,25 +47,25 @@ define(['Article', 'ArticleType'],function (Article, ArticleType) {
                location.reload()
             }
          } else {
-            let initChapterAmount = Math.ceil(500 / 15)      // 计算初始段数，非整数时向上取值
+            let initChapterAmount = Math.ceil(500 / 50)      // 计算初始段数，非整数时向上取值
 
             this.version              = 'v2.65'                 // 配置版本号
             this.chapter              = 1;                      // 当前段号
             this.chapterTotal         = initChapterAmount;      // 总段数
             this.isShuffle            = false;                  // 是否乱序模式
             this.isInEnglishMode      = false;                  // 是否处于英文打字状态
-            this.count                = '15';                   // 单条数量
+            this.count                = '50';                   // 单条数量
             this.articleName          = Article.top500.name;    // 文章名称
             this.articleIdentifier    = 'top500';               // 文章标识
             this.article              = Article.top500.content; // 文章内容
-            this.darkMode             = false;                  // 暗黑模式
+            this.darkMode             = true;                   // 暗黑模式
             this.articleType          = ArticleType.character;  // 文章类型
             this.IDBIndex             = 1;                      // IndexDB    序号
             // v2.1 自动发文
-            this.isAutoNext           = false;                  // 自动发文
-            this.isAutoRepeat         = false;                  // 重复发文
-            this.isShuffleRepeat      = false;                  // 重复发文时乱序
-            this.repeatCountTotal     = 1;                      // 总重复数
+            this.isAutoNext           = true;                   // 自动发文
+            this.isAutoRepeat         = true;                   // 重复发文
+            this.isShuffleRepeat      = true;                   // 重复发文时乱序
+            this.repeatCountTotal     = 5;                      // 总重复数
             this.repeatCountCurrent   = 1;                      // 当前重复数
             // v2.3 自定义内容
             this.customizedTitle      = '';                     // 自定义文章标题
